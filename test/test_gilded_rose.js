719a3ff4fd78b10dcb1de575611089d5ce467e22
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2019-01-02 11:31:10 +0000

    tests: add tests for Backstage Pass product

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -33,4 +33,42 @@ describe("Gilded Rose", function() {
     });
   });
 
+  describe("Backstage Passes", function() {
+
+    describe("quality", function() {
+      it("increases as SellIn value approaches", function() {
+        const backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 25);
+        const shop = new Shop([backstagePass]);
+        shop.updateQuality();
+  
+        expect(shop.items[0].quality).to.equal(26);
+      });
+
+      it("increases by 2 when sellIn is <= 10", function() {
+        const backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 25);
+        const shop = new Shop([backstagePass]);
+        shop.updateQuality();
+  
+        expect(shop.items[0].quality).to.equal(27);
+      });
+
+      it("increases by 3 when sellIn is <= 5", function() {
+        const backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 25);
+        const shop = new Shop([backstagePass]);
+        shop.updateQuality();
+  
+        expect(shop.items[0].quality).to.equal(28);
+      });
+
+      it("drops to 0 when sellIn is 0", function() {
+        const backstagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 25);
+        const shop = new Shop([backstagePass]);
+        shop.updateQuality();
+  
+        expect(shop.items[0].quality).to.equal(0);
+      });
+    });
+    
+  });
+
 });
