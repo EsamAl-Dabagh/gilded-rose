dfc978388d27ca6ecaad90e58b664ff83d112dc8
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2019-01-02 10:19:25 +0000

    tests: add test for Aged Brie product

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -2,10 +2,25 @@ var {expect} = require('chai');
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+  describe("Aged Brie", function() {
+
+    it("quality increases with age", function() {
+      const brie = new Item("Aged Brie", 30, 49);
+      const shop = new Shop([brie]);
+      shop.updateQuality();
+
+      expect(shop.items[0].quality).to.equal(50);
+    });
+
+    it("quality doesn't surpass 50", function() {
+      const brie = new Item("Aged Brie", 30, 49);
+      const shop = new Shop([brie]);
+      shop.updateQuality();
+      shop.updateQuality();
+
+      expect(shop.items[0].quality).to.equal(50);
+    });
+
   });
 
 });
